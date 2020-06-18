194931327747d020df38a324afbc21640309133d
rachit2501 <rac.sri25@gmail.com>
2020-06-19 01:26:36 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import useBookHook from "./useBookSearch";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pagenumber, setPageNumber] = useState(1);
+  const { books, hasMore, loading, error } = useBookHook(query, pagenumber);
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting ** hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      }); // detect element wrt viewport
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={query} onChange={handleSearch} />
+      {books.map((book, index) => {
+        if (books.length === index + 1)
+          return (
+            <div ref={lastBookElementRef} key={book}>
+              {book}
+            </div>
+          );
+        return <div key={book}>{book}</div>;
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "error..."}</div>
     </div>
   );
 }
